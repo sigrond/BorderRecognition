5ba5bdcbe1721719d75f0bbd9a8b1852bd66ea44
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-01-23 22:13:13 +0100

    eksperymentalnie dodany algorytm genetyczny, zmieniona interpretacja nan, więcej map funkcji celu, nowsza wersja set system

--- map3d.m
+++ map3d.m
@@ -8,7 +8,7 @@ y=linspace(-1,1,200);
 z=linspace(84.2,86.2,200);
 for i=1:200
     for j=1:200
-        point=[0,0,0,0,(j-100)*0.01,85.2+(i-100)*0.01];
+        point=[0,0,(j-100)*0.01,0,0,85.2+(i-100)*0.01];
         map2d(i,j)=BrightnesScalarization(Frame,a1,a2,point);
     end
     str=sprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\t\t%6.3f%%\n',((i-1)/2));
