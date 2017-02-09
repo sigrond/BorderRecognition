43bce60f76b70eca0810df01bb0d991fcaa04477
Tomasz Jakubczyk <sigrond93@gmail.com>
2017-02-09 13:19:37 +0100

    przyspieszenie BorderFunction - przesunięcie sprawdzania, czy jest dostępny MEX poza pętlę

--- BorderFunction.m
+++ BorderFunction.m
@@ -99,10 +99,16 @@ Br((horizontalPoints*2+verticalPoints+1):horizontalPoints*2+verticalPoints*2,3) 
 
 handles.Br=Br;
 
+if exist('RayTracing_MEX', 'file') == 3
+    RayTracing_MEX_exist=1;
+else
+    RayTracing_MEX_exist=0;
+end
+
 for i = 1:size(handles.Br,1)
        Pd = [ handles.Br(i,1), handles.Br(i,2), handles.Br(i,3) ]; % Points on the diaphragm plane 
        %P = RayTracing(Pd,handles.S);
-       if exist('RayTracing_MEX', 'file') == 3
+       if RayTracing_MEX_exist
            Ppom=RayTracing_MEX(Pd,handles);
            P=double(Ppom');
        else
